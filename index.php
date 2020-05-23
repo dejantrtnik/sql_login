6f1aa07babd61be6058fda4cede0f9ae13200c6c
dejantrtnik <dejantrtnik@gmail.com>
2020-05-23 09:33:21 +0200

    Realese

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <title>PRIMER LOGIN</title>
+<head>
+</head>
+<body>
+	<div class="">
+    <div class=""> LOGIN </div>
+		<h2>Prijava</h2>
+		<form method="POST" action="login.php"> <!-- sprememba -->
+      <div>
+        <label>Uporabniško ime</label><br>
+        <input type="text" name="username" class="form-control" value="" required>
+      </div><br>
+      <div>
+        <label>Geslo</label><br>
+        <input type="password" name="password" class="form-control" value="" required>
+      </div><br>
+      <div>
+        <button href="something.php" class="btn btn-primary"> Potrdi</button> <a style="color:black;" href="register.php"> Registracija</a>
+      </div>
+    </form>
+	</div>
+</body>
+</html>
